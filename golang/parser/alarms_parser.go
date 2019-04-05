10f14d2639d5a6712182f1db8352768e8dadbb38
Vincent Rasquier <vincent.rasquier@gmail.com>
2019-04-06 01:42:49 +0200

    Push new parsers

---
+++ golang/parser/alarms_parser.go
@@ -0,0 +1,46 @@
+package parser
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type AlarmsParser struct {
+	scanner *bufio.Scanner
+}
+
+type Alarm struct {
+	Time    string
+	Level   int
+	Message string
+}
+
+// NewAlarmsParser create a Sample Parser with the scanner
+func NewAlarmsParser(file *os.File) *AlarmsParser {
+	return &AlarmsParser{bufio.NewScanner(file)}
+}
+
+// ParseAlarms parse alarms in the file
+func (p AlarmsParser) ParseAlarms() ([]*Alarm, error) {
+	var alarms []*Alarm
+	for p.scanner.Scan() {
+		line := p.scanner.Text()
+		if len(line) < 1 {
+			return alarms, nil
+		}
+		arr := strings.Split(line, separator)
+		if len(arr) < 3 {
+			return alarms, errors.New("")
+		}
+		time := arr[0]
+		level, err := strconv.Atoi(arr[1])
+		if err != nil {
+			return alarms, err
+		}
+		alarms = append(alarms, &Alarm{Time: time, Level: level, Message: arr[2]})
+	}
+	return alarms, nil
+}
